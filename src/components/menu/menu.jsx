1bd6a1ebe9d22ac497ad5f78d753d3da243d17d4
psa231093 <design@pablosaenz.com>
2021-09-10 12:21:57 -0500

    Navigation added
    Hamburger menu added

--- src/components/menu/menu.jsx
+++ src/components/menu/menu.jsx
@@ -1,8 +1,8 @@
 import "./menu.scss";
 
-export default function menu() {
+export default function menu({ menuOpen, setMenuOpen }) {
   return (
-    <div className="menu">
+    <div className={"menu " + (menuOpen && "active")}>
       <ul>
         <li>
           <a href="#intro">Home</a>
